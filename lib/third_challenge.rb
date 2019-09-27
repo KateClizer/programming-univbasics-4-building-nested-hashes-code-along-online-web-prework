e32884f0d1b53773daadc36bf5b7099fd613a421
Katelynne Clizer <kateclizer@gmail.com>
2019-09-27 02:15:07 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -13,7 +13,10 @@ def third_challenge
         #your key/value pairs here
         name: "Romeo", age: "15", status: "alive"
         },
-      :hero_friends => []
+      :hero_friends => [
+        
+        
+        ]
    }, 
    :capulet => {
       :patriarch => {
